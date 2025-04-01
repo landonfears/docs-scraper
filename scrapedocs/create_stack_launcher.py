bd412a02e576e247527c4bec0c5b27eda6009b9f
Landon Fears <landonfears@gmail.com>
2025-03-31 20:20:57 -0400

    Bump version: 0.3.6 → 0.4.0

--- scrapedocs/create_stack_launcher.py
+++ scrapedocs/create_stack_launcher.py
@@ -20,7 +20,7 @@ def main():
         return
 
     if "--version" in sys.argv:
-        print("create-stack version 0.3.6")
+        print("create-stack version 0.4.0")
         return
 
     subprocess.run(["bash", str(script_path), *sys.argv[1:]], check=True)
